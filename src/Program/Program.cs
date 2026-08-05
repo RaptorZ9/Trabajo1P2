113f51555b3fe0581c33affee3037a59c880ec5c
CatalinaGamarra <c.gamarra@correo.ucu.edu.uy>
2026-08-05 20:02:13 -0300

    refactor class position

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,11 +7,11 @@
         Console.WriteLine(Multiplication.Multiply(5, 6));
         Console.WriteLine(Division.Divide(7, 8));
     }
-    public class Addition
+}
+ public class Addition
     {
      public static int Add(int a, int b)
       {
         return a + b;
     }
  }
-}
