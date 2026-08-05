dd198ad9a995ce614615d4ae9c2c641c83e98faa
IvanMA17 <ivan.murillo@correo.ucu.edu.uy>
2026-08-05 19:37:21 -0300

    Implementación de la operación Divide

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,4 +7,12 @@
         Console.WriteLine(Multiplication.Multiply(5, 6));
         Console.WriteLine(Division.Divide(7, 8));
     }
+    public class Division
+    {
+    public static double Divide(int a, int b)
+    {
+        return (double)a / b;
+    }
+    }
+
 }
