c07b9c8e42b2da77d1095713daf7a971fc678147
IvanMA17 <ivan.murillo@correo.ucu.edu.uy>
2026-08-10 18:53:46 -0300

    cambio incorrecto

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -3,9 +3,13 @@
     public static void Main()
     {
         Console.WriteLine(Addition.Add(1, 2));
+        // esto llama la suma
         Console.WriteLine(Subtraction.Subtract(3, 4));
+        // esto llama la resta
         Console.WriteLine(Multiplication.Multiply(5, 6));
+        // esto llama la multiplicación
         Console.WriteLine(Division.Divide(7, 8));
+        // esto llama la división + cambio incorrecto
     }
 // Esta clase implementa la operación divide    
     public class Division
